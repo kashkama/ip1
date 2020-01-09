b078fe3a93b1e99fa81f0cfaf974eb3cc58fcaeb
kashkama <owen.kamau2014@gmail.com>
2020-01-09 15:36:37 +0300

    adding the rules and options for js files install babel to bundle es6 files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
+// Webpack uses this to work with directories
 const path = require('path');
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
 module.exports = {
-	entry: './src/js/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	mode: 'developement'
-}
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/js/index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+	  rules: [{
+		  test: /\.js$/,
+		  exclude: /(node_modules)/,
+		  use: {
+			  loader: 'babel-loader',
+			  options: {
+				  presets: ['@babel/preset-env']
+			  }
+		  }
+	  }],
+  },
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'development'
+};
