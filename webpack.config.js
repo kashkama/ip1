663ad891d5b3e05212f5dd37c06262fc249bf03f
kashkama <owen.kamau2014@gmail.com>
2020-01-09 16:30:36 +0300

    adding assests loader adding plugin to minify css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // Webpack uses this to work with directories
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
@@ -30,6 +31,12 @@ module.exports = {
 		test: /\.(sa|sc|c)ss$/,
 		use: [
 			{
+				// After all CSS loaders we use plugin to do his work.
+               // It gets all transformed CSS and extracts it into separate
+               // single bundled file
+               loader: MiniCssExtractPlugin.loader
+			},
+			{
 				loader: 'css-loader',
 			},
 			{
@@ -37,13 +44,40 @@ module.exports = {
 			},
 			{
 				loader: 'sass-loader',
-				option: {
+				options: {
 					implementation: require('sass')
 				}
 			}
 		]
+	  }, {
+		  test: /\.(png|jpe?g|gif|svg)$/,
+		  use: [
+			  {
+				  loader: 'file-loader',
+				  options: {
+					  outputPath: 'images'
+				  }
+			  }
+		  ]
+	  }, {
+		// Apply rule for fonts files
+		test: /\.(woff|woff2|ttf|otf|eot)$/,
+		use: [
+			   {
+				 // Using file-loader too
+				 loader: "file-loader",
+				 options: {
+				   outputPath: 'fonts'
+				 }
+			   }
+			 ]
 	  }],
   },
+  plugins: [
+	  new MiniCssExtractPlugin({
+		  filename: 'bundle.css'
+	  })
+  ],
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript 
